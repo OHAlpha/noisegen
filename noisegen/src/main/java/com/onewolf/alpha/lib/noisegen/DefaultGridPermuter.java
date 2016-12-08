bff7929fecb24be4a2696190fe14c79845d34404
Lloyd Nugent Gibson <lnugentgibson@gmail.com>
2016-12-08 15:00:56 +0000

    implemented random grid generation

---
+++ noisegen/src/main/java/com/onewolf/alpha/lib/noisegen/DefaultGridPermuter.java
@@ -0,0 +1,22 @@
+package com.onewolf.alpha.lib.noisegen;
+
+public class DefaultGridPermuter implements GridPermuter {
+	
+	public int[] permuteGrid(int width, int height) {
+	    int[] out = new int[width * height];
+	    for( int i = 0; i < out.length; i++)
+	        out[i] = i;
+	    return out;
+	}
+	
+	public int[] permuteGrid2D(int width, int height) {
+	    int out[] = new int[2 * width * height];
+	    for( int i = 0; i < height; i++)
+	        for( int j = 0; j < width; j++) {
+	            out[2*(i*width+j)] = j;
+	            out[2*(i*width+j)+1] = i;
+	        }
+	    return out;
+	}
+	
+}
--- noisegen/src/main/java/com/onewolf/alpha/lib/noisegen/Noise.java
+++ noisegen/src/main/java/com/onewolf/alpha/lib/noisegen/Noise.java
@@ -2,6 +2,18 @@ package com.onewolf.alpha.lib.noisegen;
 
 public class Noise {
 	
+	public static int[] randomGrid(int width, int height, RandomGenerator gen, GridPermuter perm) {
+	    int[] grid = new int[width*height];
+	    int[] rand = gen.bulkInt(width*height);
+	    GridPermuter.applyGrid(GridPermuter.APPLY_METHOD_SET, width, height, rand, grid, perm);
+	    return grid;
+	}
 	
+	public static int[][] randomGrid2D(int width, int height, RandomGenerator gen, GridPermuter perm) {
+	    int[][] grid = new int[height][width];
+	    int[] rand = gen.bulkInt(width*height);
+	    GridPermuter.applyGrid2D(GridPermuter.APPLY_METHOD_SET, width, height, rand, grid, perm);
+	    return grid;
+	}
 	
 }
